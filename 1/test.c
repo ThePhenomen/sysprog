caf65d5166f4a90dee02e1c64d3c81947db33398
Dmitry Matushkin <131041543+ThePhenomen@users.noreply.github.com>
2025-02-16 21:58:43 +0300

    Decrease number of loop counter

--- 1/test.c
+++ 1/test.c
@@ -175,7 +175,7 @@ test_channel_reopen(void)
 
 	unit_msg("open and close many times");
 	c1 = coro_bus_channel_open(bus, 2);
-	for (int i = 0; i < 100000000; ++i) {
+	for (int i = 0; i < 1000; ++i) {
 		c2 = coro_bus_channel_open(bus, 2);
 		unit_assert(c2 >= 0);
 		coro_bus_channel_close(bus, c2);
